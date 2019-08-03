200026b2bae91926777a81848d04adfbce20ea2b
Shohei Yamasaki <shoyamas@yahoo-corp.jp>
2019-08-03 21:55:32 +0900

    .envは管理外にする

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-var express = require('express')
-var ejs = require('ejs')
+var express = require("express");
+var ejs = require("ejs");
 var bodyParser = require("body-parser");
+var admin = require("firebase-admin");
+require("dotenv").config();
 
-var app = express()
+var serviceAccount = require(process.env.FIREBASE_JSON_PATH)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://express-form.firebaseio.com"
+});
+
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.engine('ejs', ejs.renderFile)
+app.engine("ejs", ejs.renderFile);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs',
-    {title: "Express Form"})
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs", { title: "Express Form" });
+});
 
-app.post('/', (req, res) => {
-  res.render('request.ejs',
-    {
-      title: "Express Form",
-      body: req.body 
-  })
-})
+app.post("/", (req, res) => {
+  var db = admin.database();
+  var ref = db.ref("bookings");
+  ref.push(JSON.stringify(req.body));
 
+  res.render("request.ejs", {
+    title: "Express Form",
+    body: req.body
+  });
+});
 
 var server = app.listen(3000, () => {
-  console.log('Server is running!')
-})
+  console.log("Server is running!");
+});
