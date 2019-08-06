91952b63aae77889596be8346b2935e9e337ad65
Shohei Yamasaki <shoyamas@yahoo-corp.jp>
2019-08-06 23:20:36 +0900

    bookings

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var admin = require("firebase-admin");
 require("dotenv").config();
 
-var serviceAccount = require(process.env.FIREBASE_JSON_PATH)
+var serviceAccount = require(process.env.FIREBASE_JSON_PATH);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://express-form.firebaseio.com"
@@ -12,7 +12,7 @@ admin.initializeApp({
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.engine("ejs", ejs.renderFile);
@@ -21,10 +21,25 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { title: "Express Form" });
 });
 
+app.get("/bookings", (req, res) => {
+  var db = admin.database();
+  var dataRef = db.ref("bookings");
+  dataRef.once(
+    "value",
+    function(snapshot) {
+      console.log(snapshot.val());
+      res.render("bookings.ejs", { title: "予約一覧", bookings: snapshot.val() });
+    },
+    function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    }
+  );
+});
+
 app.post("/", (req, res) => {
   var db = admin.database();
   var ref = db.ref("bookings");
-  ref.push(JSON.stringify(req.body));
+  ref.push(req.body);
 
   res.render("request.ejs", {
     title: "Express Form",
